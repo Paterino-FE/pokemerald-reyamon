f42da8c574f629c19f1e1d47c5f24395c48cdf02
forty0 <mariusteroerde@gmx.de>
2025-03-28 23:22:38 +0100

    lilypad battle partner space center

--- src/data/battle_partners.h
+++ src/data/battle_partners.h
@@ -27,11 +27,11 @@
     [PARTNER_STEVEN] =
     {
 #line 9
-        .trainerName = _("STEVEN"),
+        .trainerName = _("Lilypad"),
 #line 10
         .trainerClass = TRAINER_CLASS_RIVAL,
 #line 11
-        .trainerPic = TRAINER_BACK_PIC_STEVEN,
+        .trainerPic = TRAINER_BACK_PIC_WALLY,
         .encounterMusic_gender = 
 #line 13
             TRAINER_ENCOUNTER_MUSIC_MALE,
@@ -40,65 +40,68 @@
         {
             {
 #line 15
-            .species = SPECIES_METANG,
+            .species = SPECIES_LUDICOLO,
             .gender = TRAINER_MON_RANDOM_GENDER,
 #line 19
             .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
 #line 18
             .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
 #line 17
-            .lvl = 42,
+            .lvl = 41,
 #line 16
-            .nature = NATURE_BRAVE,
+            .nature = NATURE_HARDY,
             .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+            .ability = ABILITY_RAIN_DISH,
             .moves = {
 #line 20
-                MOVE_LIGHT_SCREEN,
-                MOVE_PSYCHIC,
-                MOVE_REFLECT,
-                MOVE_METAL_CLAW,
+                MOVE_RAIN_DANCE,
+                MOVE_SCALD,
+                MOVE_ENERGY_BALL,
+                MOVE_ICE_BEAM,
             },
             },
             {
 #line 25
-            .species = SPECIES_SKARMORY,
+            .species = SPECIES_FLOATZEL,
             .gender = TRAINER_MON_RANDOM_GENDER,
 #line 29
             .ev = TRAINER_PARTY_EVS(252, 0, 0, 0, 6, 252),
 #line 28
             .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
 #line 27
-            .lvl = 43,
+            .lvl = 42,
 #line 26
-            .nature = NATURE_IMPISH,
+            .nature = NATURE_HARDY,
             .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+            .ability = ABILITY_SWIFT_SWIM,
+            .friendship = 255,
             .moves = {
 #line 30
-                MOVE_TOXIC,
-                MOVE_AERIAL_ACE,
-                MOVE_PROTECT,
-                MOVE_STEEL_WING,
+                MOVE_AQUA_JET,
+                MOVE_PURSUIT,
+                MOVE_RETURN,
+                MOVE_ENCORE,
             },
             },
             {
 #line 35
-            .species = SPECIES_AGGRON,
+            .species = SPECIES_SAMUROTT,
             .gender = TRAINER_MON_RANDOM_GENDER,
 #line 39
             .ev = TRAINER_PARTY_EVS(0, 252, 0, 0, 252, 6),
 #line 38
             .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
 #line 37
-            .lvl = 44,
+            .lvl = 43,
 #line 36
             .nature = NATURE_ADAMANT,
             .dynamaxLevel = MAX_DYNAMAX_LEVEL,
             .moves = {
 #line 40
-                MOVE_THUNDER,
-                MOVE_PROTECT,
-                MOVE_SOLAR_BEAM,
-                MOVE_DRAGON_CLAW,
+                MOVE_AQUA_JET,
+                MOVE_ENCORE,
+                MOVE_REVENGE,
+                MOVE_SCALD,
             },
             },
         },
