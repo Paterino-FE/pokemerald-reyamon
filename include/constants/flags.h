15dbfc767551a79b0acc5541830c95bf2f630a5a
forty0 <mariusteroerde@gmx.de>
2025-04-14 19:54:18 +0200

    Post champion fight dialogues reworked

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1228,7 +1228,7 @@
 #define FLAG_HIDE_TRUBBISH_SHOP                                     0x496 
 #define FLAG_HIDE_SLOWPOKE                                          0x497 // Unused Flag
 #define FLAG_ENABLE_PATERINO_STARTER                                0x498 // Unused Flag
-#define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
+#define FLAG_HIDE_CHAMP_VYRAL                                       0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
 #define FLAG_UNUSED_0x49C                                           0x49C // Unused Flag
