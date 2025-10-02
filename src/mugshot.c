8f726deb7648b328605bdd076e43fd4bdbfb28f1
Paterino-FE <pm@jugdral.de>
2025-10-02 14:20:10 +0200

    Fixed typos in mugshot.c

--- src/mugshot.c
+++ src/mugshot.c
@@ -53,8 +53,8 @@ static const u32 sMugshotImg_Skyward[] = INCBIN_U32("graphics/mugshots/leader_ta
 static const u16 sMugshotPal_Skyward[] = INCBIN_U16("graphics/mugshots/leader_tate_and_liza.gbapal");
 static const u32 sMugshotImg_Lumi[] = INCBIN_U32("graphics/mugshots/leader_juan.4bpp.lz");
 static const u16 sMugshotPal_Lumi[] = INCBIN_U16("graphics/mugshots/leader_juan.gbapal");
-static const u32 sMugshotImg_Arle[] = INCBIN_U32("graphics/mugshots/elite_four_sydney.4bpp.lz");
-static const u16 sMugshotPal_Arle[] = INCBIN_U16("graphics/mugshots/elite_four_sydney.gbapal");
+static const u32 sMugshotImg_Arle[] = INCBIN_U32("graphics/mugshots/elite_four_sidney.4bpp.lz");
+static const u16 sMugshotPal_Arle[] = INCBIN_U16("graphics/mugshots/elite_four_sidney.gbapal");
 static const u32 sMugshotImg_Astarion[] = INCBIN_U32("graphics/mugshots/elite_four_phoebe.4bpp.lz");
 static const u16 sMugshotPal_Astarion[] = INCBIN_U16("graphics/mugshots/elite_four_phoebe.gbapal");
 static const u32 sMugshotImg_Cynthia[] = INCBIN_U32("graphics/mugshots/elite_four_glacia.4bpp.lz");
@@ -96,7 +96,7 @@ static const struct Mugshot sMugshots[] = {
     [MUGSHOT_GABE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Gabe, .palette = sMugshotPal_Gabe},
     [MUGSHOT_ANGE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Ange, .palette = sMugshotPal_Ange},
     [MUGSHOT_THEA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Thea, .palette = sMugshotPal_Thea},
-    [MUGSHOT_SKYWARD] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Skyward .palette = sMugshotPal_Skyward},
+    [MUGSHOT_SKYWARD] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Skyward, .palette = sMugshotPal_Skyward},
     [MUGSHOT_LUMI] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lumi, .palette = sMugshotPal_Lumi},
     [MUGSHOT_ARLE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Arle, .palette = sMugshotPal_Arle},
     [MUGSHOT_ASTARION] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Astarion, .palette = sMugshotPal_Astarion},
@@ -107,7 +107,7 @@ static const struct Mugshot sMugshots[] = {
     [MUGSHOT_HASTINGS] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Hastings, .palette = sMugshotPal_Hastings},
     [MUGSHOT_DP] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_DP, .palette = sMugshotPal_DP},
     [MUGSHOT_LYRA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lyra, .palette = sMugshotPal_Lyra},
-    [MUGSHOT_SVELTE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Svelte .palette = sMugshotPal_Svelte},
+    [MUGSHOT_SVELTE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Svelte, .palette = sMugshotPal_Svelte},
     [MUGSHOT_GRUNT_MAGMA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Magma_Grunt, .palette = sMugshotPal_Magma_Grunt},
     [MUGSHOT_FRIEREN] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Frieren, .palette = sMugshotPal_Frieren},
     [MUGSHOT_REYABS] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Reyabs, .palette = sMugshotPal_Reyabs},
