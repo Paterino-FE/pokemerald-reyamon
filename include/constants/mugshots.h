39aee9c8ddcd4b79c03b1869d546f5b3fdfe7e7c
Felixr2 <71655782+Felixr22@users.noreply.github.com>
2025-10-02 13:22:32 +0200

    Implemented mugshots

--- include/constants/mugshots.h
+++ include/constants/mugshots.h
@@ -2,8 +2,35 @@
 #define GUARD_CONSTANTS_MUGSHOT_H
 
 //Add your mugshot constants here
-#define MUGSHOT_REYA        0
+#define MUGSHOT_REYADETAIL  0
 #define MUGSHOT_ROSUURI     1
-#define MUGSHOT_WALLY       2
+#define MUGSHOT_LILYPAD     2
+#define MUGSHOT_VYRAL       3
+#define MUGSHOT_LILYPAL     4
+#define MUGSHOT_DIRKLING    5
+#define MUGSHOT_EMILIA      6
+#define MUGSHOT_ATSUI       7
+#define MUGSHOT_HAMS        8
+#define MUGSHOT_CLOR        9
+#define MUGSHOT_DALEK       10
+#define MUGSHOT_GABE        11
+#define MUGSHOT_ANGE        12
+#define MUGSHOT_THEA        13
+#define MUGSHOT_SKYWARD     14
+#define MUGSHOT_LUMI        15
+#define MUGSHOT_ARLE        16
+#define MUGSHOT_ASTARION    17
+#define MUGSHOT_CYNTHIA     18
+#define MUGSHOT_SATSUKI     19
+#define MUGSHOT_FORTY       20
+#define MUGSHOT_HATZOOME    21
+#define MUGSHOT_HASTINGS    22
+#define MUGSHOT_DP          23
+#define MUGSHOT_LYRA        24
+#define MUGSHOT_SVELTE      25
+#define MUGSHOT_GRUNT_MAGMA 26
+#define MUGSHOT_FRIEREN     27
+#define MUGSHOT_REYABS      28
+#define MUGSHOT_ALEX        29
 
 #endif
--- src/mugshot.c
+++ src/mugshot.c
@@ -25,15 +25,93 @@ static const u32 sMugshotImg_Reya[] = INCBIN_U32("graphics/mugshots/reya.4bpp.lz
 static const u16 sMugshotPal_Reya[] = INCBIN_U16("graphics/mugshots/reya.gbapal");
 static const u32 sMugshotImg_Rosuuri[] = INCBIN_U32("graphics/mugshots/rosuuri.4bpp.lz");
 static const u16 sMugshotPal_Rosuuri[] = INCBIN_U16("graphics/mugshots/rosuuri.gbapal");
-static const u32 sMugshotImg_Wally[] = INCBIN_U32("graphics/mugshots/wally.4bpp.lz");
-static const u16 sMugshotPal_Wally[] = INCBIN_U16("graphics/mugshots/wally.gbapal");
+static const u32 sMugshotImg_Lilypad[] = INCBIN_U32("graphics/mugshots/wally.4bpp.lz");
+static const u16 sMugshotPal_Lilypad[] = INCBIN_U16("graphics/mugshots/wally.gbapal");
+static const u32 sMugshotImg_Vyral[] = INCBIN_U32("graphics/mugshots/brendan.4bpp.lz");
+static const u16 sMugshotPal_Vyral[] = INCBIN_U16("graphics/mugshots/brendan.gbapal");
+static const u32 sMugshotImg_Lilypal[] = INCBIN_U32("graphics/mugshots/lilypal.4bpp.lz");
+static const u16 sMugshotPal_Lilypal[] = INCBIN_U16("graphics/mugshots/lilypal.gbapal");
+static const u32 sMugshotImg_Dirkling[] = INCBIN_U32("graphics/mugshots/dirkling.4bpp.lz");
+static const u16 sMugshotPal_Dirkling[] = INCBIN_U16("graphics/mugshots/dirkling.gbapal");
+static const u32 sMugshotImg_Emilia[] = INCBIN_U32("graphics/mugshots/emilia.4bpp.lz");
+static const u16 sMugshotPal_Emilia[] = INCBIN_U16("graphics/mugshots/emilia.gbapal");
+static const u32 sMugshotImg_Atsui[] = INCBIN_U32("graphics/mugshots/salon_maiden_anabel.4bpp.lz");
+static const u16 sMugshotPal_Atsui[] = INCBIN_U16("graphics/mugshots/salon_maiden_anabel.gbapal");
+static const u32 sMugshotImg_Hams[] = INCBIN_U32("graphics/mugshots/leader_brawly.4bpp.lz");
+static const u16 sMugshotPal_Hams[] = INCBIN_U16("graphics/mugshots/leader_brawly.gbapal");
+static const u32 sMugshotImg_Clor[] = INCBIN_U32("graphics/mugshots/leader_clor.4bpp.lz");
+static const u16 sMugshotPal_Clor[] = INCBIN_U16("graphics/mugshots/leader_clor.gbapal");
+static const u32 sMugshotImg_Dalek[] = INCBIN_U32("graphics/mugshots/leader_wattson.4bpp.lz");
+static const u16 sMugshotPal_Dalek[] = INCBIN_U16("graphics/mugshots/leader_wattson.gbapal");
+static const u32 sMugshotImg_Gabe[] = INCBIN_U32("graphics/mugshots/leader_flannery.4bpp.lz");
+static const u16 sMugshotPal_Gabe[] = INCBIN_U16("graphics/mugshots/leader_flannery.gbapal");
+static const u32 sMugshotImg_Ange[] = INCBIN_U32("graphics/mugshots/leader_norman.4bpp.lz");
+static const u16 sMugshotPal_Ange[] = INCBIN_U16("graphics/mugshots/leader_norman.gbapal");
+static const u32 sMugshotImg_Thea[] = INCBIN_U32("graphics/mugshots/leader_winona.4bpp.lz");
+static const u16 sMugshotPal_Thea[] = INCBIN_U16("graphics/mugshots/leader_winona.gbapal");
+static const u32 sMugshotImg_Skyward[] = INCBIN_U32("graphics/mugshots/leader_tate_and_liza.4bpp.lz");
+static const u16 sMugshotPal_Skyward[] = INCBIN_U16("graphics/mugshots/leader_tate_and_liza.gbapal");
+static const u32 sMugshotImg_Lumi[] = INCBIN_U32("graphics/mugshots/leader_juan.4bpp.lz");
+static const u16 sMugshotPal_Lumi[] = INCBIN_U16("graphics/mugshots/leader_juan.gbapal");
+static const u32 sMugshotImg_Arle[] = INCBIN_U32("graphics/mugshots/elite_four_sydney.4bpp.lz");
+static const u16 sMugshotPal_Arle[] = INCBIN_U16("graphics/mugshots/elite_four_sydney.gbapal");
+static const u32 sMugshotImg_Astarion[] = INCBIN_U32("graphics/mugshots/elite_four_phoebe.4bpp.lz");
+static const u16 sMugshotPal_Astarion[] = INCBIN_U16("graphics/mugshots/elite_four_phoebe.gbapal");
+static const u32 sMugshotImg_Cynthia[] = INCBIN_U32("graphics/mugshots/elite_four_glacia.4bpp.lz");
+static const u16 sMugshotPal_Cynthia[] = INCBIN_U16("graphics/mugshots/elite_four_glacia.gbapal");
+static const u32 sMugshotImg_Satsuki[] = INCBIN_U32("graphics/mugshots/elite_four_drake.4bpp.lz");
+static const u16 sMugshotPal_Satsuki[] = INCBIN_U16("graphics/mugshots/elite_four_drake.gbapal");
+static const u32 sMugshotImg_Forty[] = INCBIN_U32("graphics/mugshots/blaine_front_pic.4bpp.lz");
+static const u16 sMugshotPal_Forty[] = INCBIN_U16("graphics/mugshots/blaine_front_pic.gbapal");
+static const u32 sMugshotImg_Hatzoome[] = INCBIN_U32("graphics/mugshots/aqua_leader_archie.4bpp.lz");
+static const u16 sMugshotPal_Hatzoome[] = INCBIN_U16("graphics/mugshots/aqua_leader_archie.gbapal");
+static const u32 sMugshotImg_Hastings[] = INCBIN_U32("graphics/mugshots/aqua_admin_m.4bpp.lz");
+static const u16 sMugshotPal_Hastings[] = INCBIN_U16("graphics/mugshots/aqua_admin_m.gbapal");
+static const u32 sMugshotImg_DP[] = INCBIN_U32("graphics/mugshots/aqua_admin_f.4bpp.lz");
+static const u16 sMugshotPal_DP[] = INCBIN_U16("graphics/mugshots/aqua_admin_f.gbapal");
+static const u32 sMugshotImg_Lyra[] = INCBIN_U32("graphics/mugshots/magma_leader_maxie.4bpp.lz");
+static const u16 sMugshotPal_Lyra[] = INCBIN_U16("graphics/mugshots/magma_leader_maxie.gbapal");
+static const u32 sMugshotImg_Svelte[] = INCBIN_U32("graphics/mugshots/magma_admin.4bpp.lz");
+static const u16 sMugshotPal_Svelte[] = INCBIN_U16("graphics/mugshots/magma_admin.gbapal");
+static const u32 sMugshotImg_Magma_Grunt[] = INCBIN_U32("graphics/mugshots/magma_grunt_f.4bpp.lz");
+static const u16 sMugshotPal_Magma_Grunt[] = INCBIN_U16("graphics/mugshots/magma_grunt_f.gbapal");
+static const u32 sMugshotImg_Frieren[] = INCBIN_U32("graphics/mugshots/frieren.4bpp.lz");
+static const u16 sMugshotPal_Frieren[] = INCBIN_U16("graphics/mugshots/frieren.gbapal");
+static const u32 sMugshotImg_Reyabs[] = INCBIN_U32("graphics/mugshots/may.4bpp.lz");
+static const u16 sMugshotPal_Reyabs[] = INCBIN_U16("graphics/mugshots/may.gbapal");
 
 static const struct Mugshot sMugshots[] = {
     //ADD YOUR MUGSHOTS HERE
-    [MUGSHOT_REYA] = {.x = 16, .y = 0, .width = 64, .height = 64, .image = sMugshotImg_Reya, .palette = sMugshotPal_Reya},
+    [MUGSHOT_REYADETAIL] = {.x = 16, .y = 0, .width = 64, .height = 64, .image = sMugshotImg_Reya, .palette = sMugshotPal_Reya},
     [MUGSHOT_ROSUURI] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Rosuuri, .palette = sMugshotPal_Rosuuri},
-    [MUGSHOT_WALLY] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Wally, .palette = sMugshotPal_Wally},
-};
+    [MUGSHOT_LILYPAD] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lilypad, .palette = sMugshotPal_Lilypad},
+    [MUGSHOT_VYRAL] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Vyral, .palette = sMugshotPal_Vyral},
+    [MUGSHOT_LILYPAL] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lilypal, .palette = sMugshotPal_Lilypal},
+    [MUGSHOT_DIRKLING] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Dirkling, .palette = sMugshotPal_Dirkling},
+    [MUGSHOT_EMILIA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Emilia, .palette = sMugshotPal_Emilia},
+    [MUGSHOT_ATSUI] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Atsui, .palette = sMugshotPal_Atsui},
+    [MUGSHOT_HAMS] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Hams, .palette = sMugshotPal_Hams},
+    [MUGSHOT_CLOR] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Clor, .palette = sMugshotPal_Clor},
+    [MUGSHOT_DALEK] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Dalek, .palette = sMugshotPal_Dalek},
+    [MUGSHOT_GABE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Gabe, .palette = sMugshotPal_Gabe},
+    [MUGSHOT_ANGE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Ange, .palette = sMugshotPal_Ange},
+    [MUGSHOT_THEA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Thea, .palette = sMugshotPal_Thea},
+    [MUGSHOT_SKYWARD] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Skyward .palette = sMugshotPal_Skyward},
+    [MUGSHOT_LUMI] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lumi, .palette = sMugshotPal_Lumi},
+    [MUGSHOT_ARLE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Arle, .palette = sMugshotPal_Arle},
+    [MUGSHOT_ASTARION] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Astarion, .palette = sMugshotPal_Astarion},
+    [MUGSHOT_CYNTHIA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Cynthia, .palette = sMugshotPal_Cynthia},
+    [MUGSHOT_SATSUKI] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Satsuki, .palette = sMugshotPal_Satsuki},
+    [MUGSHOT_FORTY] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Forty, .palette = sMugshotPal_Forty},
+    [MUGSHOT_HATZOOME] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Hatzoome, .palette = sMugshotPal_Hatzoome},
+    [MUGSHOT_HASTINGS] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Hastings, .palette = sMugshotPal_Hastings},
+    [MUGSHOT_DP] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_DP, .palette = sMugshotPal_DP},
+    [MUGSHOT_LYRA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Lyra, .palette = sMugshotPal_Lyra},
+    [MUGSHOT_SVELTE] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Svelte .palette = sMugshotPal_Svelte},
+    [MUGSHOT_GRUNT_MAGMA] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Magma_Grunt, .palette = sMugshotPal_Magma_Grunt},
+    [MUGSHOT_FRIEREN] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Frieren, .palette = sMugshotPal_Frieren},
+    [MUGSHOT_REYABS] = {.x = 20, .y = 6, .width = 64, .height = 64, .image = sMugshotImg_Reyabs, .palette = sMugshotPal_Reyabs},
+}; 
 
 
 //WindowId + 1, 0 if window is not open
