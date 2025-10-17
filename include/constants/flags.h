6452f159decbc34415e39d689c6521505017d5a1
forty0 <mariusteroerde@gmx.de>
2025-10-17 19:35:25 +0200

    atsui dialogue

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1175,7 +1175,7 @@
 #define FLAG_HIDE_SAMUROTT                                          0x4A1 // Unused Flag
 #define FLAG_HIDE_OSHAWOTT_2                                        0x4A2 // Unused Flag
 #define FLAG_WON_ROCKPAPERSCISSORS                                  0x4A3 // Unused Flag
-#define FLAG_UNUSED_0x4A4                                           0x4A4 // Unused Flag
+#define FLAG_TALKED_TO_ATSUI                                        0x4A4 // Unused Flag
 #define FLAG_UNUSED_0x4A5                                           0x4A5 // Unused Flag
 #define FLAG_UNUSED_0x4A6                                           0x4A6 // Unused Flag
 #define FLAG_UNUSED_0x4A7                                           0x4A7 // Unused Flag
