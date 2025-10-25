1b9504db74db946886ad2bd2cda684b157e54cb1
forty0 <mariusteroerde@gmx.de>
2025-10-25 11:28:43 +0200

    hatzoome ending

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1247,8 +1247,8 @@
 #define FLAG_GIVEN_AIR_BALLOON                                      0x4E9 // Unused Flag
 #define FLAG_TALKED_HATZOOME_LANNN                                  0x4EA // Unused Flag
 #define FLAG_TALKED_HATZOOME_PRESENT                                0x4EB // Unused Flag
-#define FLAG_UNUSED_0x4EC                                           0x4EC // Unused Flag
-#define FLAG_UNUSED_0x4ED                                           0x4ED // Unused Flag
+#define FLAG_TALKED_HATZOOME_ENDING                                 0x4EC // Unused Flag
+#define FLAG_HIDE_HATZOOME_ENDING                                   0x4ED // Unused Flag
 #define FLAG_UNUSED_0x4EE                                           0x4EE // Unused Flag
 #define FLAG_UNUSED_0x4EF                                           0x4EF // Unused Flag
 
