0221db1f41cadd4c83b43f87b3d5b3b952f10ec9
paterino-fe <pm@jugdral.de>
2025-10-25 15:09:32 +0200

    Created new level caps for Alex, Vyral, FInalBoss

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1323,9 +1323,9 @@
 #define FLAG_IS_CHAMPION                            (SYSTEM_FLAGS + 0x1F) // Seems to be related to linking.
 #define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x20)
 
-#define FLAG_UNUSED_0x881                           (SYSTEM_FLAGS + 0x21) // Unused Flag
-#define FLAG_UNUSED_0x882                           (SYSTEM_FLAGS + 0x22) // Unused Flag
-#define FLAG_UNUSED_0x883                           (SYSTEM_FLAGS + 0x23) // Unused Flag
+#define FLAG_BEAT_ALEX                              (SYSTEM_FLAGS + 0x21) // Unused Flag
+#define FLAG_BEAT_VYRAL                             (SYSTEM_FLAGS + 0x22) // Unused Flag
+#define FALG_BEAT_IMPOSTERPAD                       (SYSTEM_FLAGS + 0x23) // Unused Flag
 #define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
 #define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
 #define FLAG_UNUSED_0x886                           (SYSTEM_FLAGS + 0x26) // Unused Flag
